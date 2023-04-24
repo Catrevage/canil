e3250de5cba66ed06690c44edb46e00cb703e2af
Catrevage <damiaovcjr@hotmail.com>
2023-04-24 09:46:34 -0300

    criação das totas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import * as PageController from "../controllers/pageController";
+import * as SearchController from "../controllers/searchController";
 
 const route = Router();
 
-route.get('/', (req, res) =>{
-    res.send('home');
-});
+route.get('/', PageController.home);
+route.get('/dogs', PageController.dogs);
+route.get('/cats', PageController.cats);
+route.get('/fishes', PageController.fishes);
+
+route.get('/search', SearchController.search);
+
 
 export default route;
--- src/server.ts
+++ src/server.ts
@@ -3,20 +3,23 @@ import env from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
 import routes from './routes/index'
+
 env.config();
 
 const server = express();
 
+//template engine
 server.set('view engine', 'mustache');
 server.set('view', path.join(__dirname, 'views'));
 server.engine('mustache', mustache());
+
 
 server.use(express.static(path.join(__dirname, '../public')));
 
 //routes
 server.use(routes)
 server.use((req, res) =>{
-    res.send('Páfina não encontrada');
+    res.send('Página não encontrada');
 })
 
 
